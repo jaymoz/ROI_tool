7384ce7f97ad097729bbcddcfab808929ad14d6f
Noopur Zambare <92505473+noopur-zambare@users.noreply.github.com>
2024-07-24 00:27:16 +0530

    graying effect added

--- client/src/components/Dashboard/MLConfig.js
+++ client/src/components/Dashboard/MLConfig.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ActiveLearning from './ActiveLearning';
 import axios from 'axios';
 import Select from 'react-select';
 import './MLConfig.css';
 import './Dashboard_sidebar.css';
-import arrow_key from '../images/left_arrow.png';
 import Results from './Results';
 import './ImportCSV.css';
 
@@ -21,155 +20,145 @@ const MLdropdown = () => {
     const [tp, setTP] = useState(false);
     const [cv_mean, setCV] = useState(null);
     const [baseModel, setBaseModel] = useState(null); 
-
-
     const [supervisedSubOption, setSupervisedSubOption] = useState(null);
+    const [isLearningSelected, setIsLearningSelected] = useState(false);
+
     const baseModelOptions = [
         { value: 'supervised', label: 'Supervised Learning' },
         { value: 'activeLearning', label: 'Active Learning' }
     ];
 
     const supervisedSubOptions = [
-        { value: 'logistic_regression', label: 'logistic_regression' },
-        { value: 'naive_bayes', label: 'naive_bayes' },
-        { value: 'random_forest', label: 'random_forest' },
-        { value: 'support_vector_machine', label: 'support_vector_machine' },
-        { value: 'decision_tree', label: 'decision_tree' }
+        { value: 'logistic_regression', label: 'Logistic Regression' },
+        { value: 'naive_bayes', label: 'Naive Bayes' },
+        { value: 'random_forest', label: 'Random Forest' },
+        { value: 'support_vector_machine', label: 'Support Vector Machine' },
+        { value: 'decision_tree', label: 'Decision Tree' }
     ];
 
-
     const handleModelChange = (selectedOption) => {
-        setBaseModel(selectedOption.value);
-        console.log(baseModel);
+        setBaseModel(selectedOption);
+        console.log(selectedOption.value);
         handleLearningSelect(selectedOption.value);
     };
 
     const handleSupervisedSubModelChange = (selectedOption) => {
-        setSupervisedSubOption(selectedOption.value);
-        console.log(supervisedSubOption);
-        handleOptionSelect(supervisedSubOption);
+        setSupervisedSubOption(selectedOption);
+        console.log(selectedOption.value);
+        handleOptionSelect(selectedOption.value);
     };
 
-  const handleLearningSelect = async (selectedValue) => {
-    console.log('Selected option:', selectedValue);
+    const handleLearningSelect = async (selectedValue) => {
+        console.log('Selected option:', selectedValue);
+        setIsLearningSelected(true); // Set the variable to true if any learning option is selected
 
-    const headers = {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-        "X-Requested-With": "XMLHttpRequest"  // Required by some versions of cors-anywhere
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+            "X-Requested-With": "XMLHttpRequest"
+        };
+
+        if (selectedValue === 'supervised') {
+            try {
+                const response = await axios.post('https://roibackend.shaktilab.org/weekly-supervised', {}, { headers: headers });
+                console.log(response.data);
+                if (response.data.success) {
+                    setLearning('supervised');
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        } else if (selectedValue === 'activeLearning') {
+            try {
+                const response = await axios.post('https://roibackend.shaktilab.org/active-learning', {}, { headers: headers });
+                console.log(response.data);
+                if (response.data.success) {
+                    setLearning('activeLearning');
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
     };
 
-    if (selectedValue === 'supervised') {
-      try {
-        const response = await axios.post('https://roibackend.shaktilab.org/weekly-supervised', {}, { headers: headers });
-        console.log(response.data);
+    const handleOptionSelect = async (selectedValue) => {
+        console.log('Selected option:', selectedValue);
 
-        if (response.data.success) {
-          setLearning('supervised');
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+            "X-Requested-With": "XMLHttpRequest"
+        };
+
+        let endpoint = "";
+        switch (selectedValue) {
+            case 'logistic_regression':
+                endpoint = 'logistic-regression';
+                break;
+            case 'naive_bayes':
+                endpoint = 'naive-bayes';
+                break;
+            case 'random_forest':
+                endpoint = 'random-forest';
+                break;
+            case 'support_vector_machine':
+                endpoint = 'support-vector-machine';
+                break;
+            case 'decision_tree':
+                endpoint = 'decision-tree';
+                break;
+            default:
+                return;
         }
-      } catch (error) {
-        console.error(error);
-      }
-    } else if (selectedValue === 'activeLearning') {
-      try {
-        const response = await axios.post('https://roibackend.shaktilab.org/active-learning', {}, { headers: headers });
-        console.log(response.data);
-        if (response.data.success) {
-          setLearning('activeLearning');
+
+        try {
+            const response = await axios.post(`https://roibackend.shaktilab.org/${endpoint}`, {}, { headers: headers });
+            console.log(`https://roibackend.shaktilab.org/${endpoint}`);
+            console.log(response.data);
+            if (response.data.success) {
+                setReport(response.data.report);
+                setAccuracy(response.data.accuracy);
+                setStopTime(response.data.stop);
+                setGraph(response.data.graph);
+                setConfusionMatrix(response.data.cm);
+                setF1Score(response.data.f1);
+                setFP(response.data.fp);
+                setFN(response.data.fn);
+                setTP(response.data.tp);
+                setCV(response.data.cv_mean);
+            }
+        } catch (error) {
+            console.error(error);
         }
-      } catch (error) {
-        console.error(error);
-      }
-    }
-};
+    };
 
-
-const handleOptionSelect = async (selectedValue) => {
-  console.log('Selected option:', selectedValue);
-
-  const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-      "X-Requested-With": "XMLHttpRequest"
-  };
-
-  let endpoint = "";
-  switch (selectedValue) {
-      case 'logistic_regression':
-          endpoint = 'logistic-regression';
-          break;
-      case 'naive_bayes':
-          endpoint = 'naive-bayes';
-          break;
-      case 'random_forest':
-          endpoint = 'random-forest';
-          break;
-      case 'support_vector_machine':
-          endpoint = 'support-vector-machine';
-          break;
-      case 'decision_tree':
-          endpoint = 'decision-tree';
-          break;
-      default:
-          return;
-  }
-
-  try {
-      const response = await axios.post(`https://roibackend.shaktilab.org/${endpoint}`, {}, { headers: headers });
-      
-      console.log(`https://roibackend.shaktilab.org/${endpoint}`);
-      console.log(response.data);
-      if (response.data.success) {
-          setReport(response.data.report);
-          setAccuracy(response.data.accuracy);
-          setStopTime(response.data.stop);
-          setGraph(response.data.graph);
-          setConfusionMatrix(response.data.cm);
-          setF1Score(response.data.f1);
-          setFP(response.data.fp);
-          setFN(response.data.fn);
-          setTP(response.data.tp);
-          setCV(response.data.cv_mean);
-      }
-  } catch (error) {
-      console.error(error);
-  }
-};
-
-  return (
-        <div className="container ">
+    return (
+        <div className="container">
             <div className='section'>
-            <div className='upload-sidebar subsection'>
-                <Select
-                    options={baseModelOptions}
-                    value={baseModel}
-                    onChange={handleModelChange}
-                    placeholder="ML Type"
-                    className='ml-sidebar-item'
-                />
-                {
-                    baseModel === 'supervised' && 
+                <div className='upload-sidebar subsection'>
                     <Select
-                        options={supervisedSubOptions}
-                        value={supervisedSubOption}
-                        onChange={handleSupervisedSubModelChange}
-                        placeholder="Sub Model"
+                        options={baseModelOptions}
+                        value={baseModel}
+                        onChange={handleModelChange}
+                        placeholder="ML Type"
                         className='ml-sidebar-item'
                     />
-                }
-                {
-                    baseModel === 'activeLearning' && 
-                        <ActiveLearning />
-                }
-
-            </div>
+                    {baseModel && baseModel.value === 'supervised' && (
+                        <Select
+                            options={supervisedSubOptions}
+                            value={supervisedSubOption}
+                            onChange={handleSupervisedSubModelChange}
+                            placeholder="Sub Model"
+                            className='ml-sidebar-item'
+                        />
+                    )}
+                    {baseModel && baseModel.value === 'activeLearning' && <ActiveLearning />}
+                </div>
             </div>
             
             <div className="ml-table-section section" style={{color: '#28a9e2'}}>
-                {learning === 'supervised' && (
-                    /* supervised models and report */
+                {learning === 'supervised' && report && (
                     <React.Fragment>
-                    {report && (
                         <div className="subsection report-subsection">
                             <div className="report-content">
                                 <div className='accuracy-report-subsection report-inside-item'>
@@ -199,19 +188,17 @@ const handleOptionSelect = async (selectedValue) => {
                                         {report}
                                     </div>
                                 </pre>
-                                
 
-                            <img src={graph} />
-                            <img src={confusionMatrix}/>
+                                <img src={graph} alt="Graph" />
+                                <img src={confusionMatrix} alt="Confusion Matrix"/>
                             </div>
                         </div>
-                    )}
                     </React.Fragment>
                 )}
-                <Results/>
+                <Results isLearningSelected={isLearningSelected} />
             </div>
         </div>
-  );
+    );
 };
 
 export default MLdropdown;
